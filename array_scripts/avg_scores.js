f21b647917cbb60ff11f0be7d2681520519f6026
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-09 09:43:11 -0500

    food js

--- array_scripts/avg_scores.js
+++ array_scripts/avg_scores.js
@@ -3,6 +3,23 @@
 let myScores = [92,98,84,76, 89,99,100];
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
+
+// a function to calculate an average from a list of numbers
+function getAverage(scores){
+
+    // create a variable to keep track of our total of all the number in the array
+    let total = 0;
+    // a loop
+    for(let i =0; i<scores.length;i++){
+        total += scores[i]; 
+    }
+    
+    return total / scores.length;
+}
+console.log(getAverage(myScores));
+console.log(getAverage(yourScores));
+
+
 // function getAverage(scores){
 //     let total = 0;
 //     for(let i =0; i<myScores.length;i++){
@@ -22,16 +39,3 @@ let yourScores = [82, 98, 94, 88, 92, 100, 100];
    
 // }
 //  console.log(average(yourScores));
-
-function getAverage(scores){
-    let total = 0;
-    for(let i =0; i<scores.length;i++){
-        total += scores[i]; 
-    }
-    
-    return total / scores.length;
-}
-console.log(getAverage(myScores));
-console.log(getAverage(yourScores));
-
-
--- array_scripts/foods.js
+++ array_scripts/foods.js
@@ -1,16 +1,29 @@
-
+"use strict"
 let lunch = [
     {item: "keiki plate",price:6.00},
     {item:"coco Shrimp", price:12.25},
     {item:"Mochi Ice Cream", price:2.00}
 ]
-function takeOut(items){
+function takeOutAmount(meals){
+    // start a variable to keep track of total
     let totalcost = 0;
+    // let numberOfMeals = mealsArray.length
 
-    for(let i = 0; i < items.length; i++){
-        totalcost += items[i].price;
+    for(let i = 0; i < meals.length; i++){
+        totalcost += meals[i].price;
     }
+
     return totalcost;
     
 }
-console.log(takeOut(lunch));
+let lunchTotal = takeOutAmount(lunch);
+let lunchTax = lunchTotal * (8/100);
+let lunchTip = lunchTotal * (18/100);
+let totalDue = lunchTotal +lunchTax +lunchTip;
+
+console.log("the lunch total due is: "+lunchTotal); 
+console.log("the tax due is: "+lunchTax); 
+console.log("the tip due is: "+lunchTip); 
+console.log("the total due is: "+ totalDue.toFixed(2));
+
+                       
