3cc62b8458f0af63596acf9cdadb72dba974df31
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-10 11:56:44 -0500

    added course sorting

---
+++ sorting/courses_sorting.js
@@ -0,0 +1,58 @@
+"use strict"
+
+let courses = [
+    {
+        CourseId: "PROG100",
+        Title: "Introduction to HTML/CSS/Git",
+        Location: "Classroom 7",
+        StartDate: "09/08/22",
+        Fee: "100.00",
+    },
+    {
+        CourseId: "PROG200",
+        Title: "Introduction to JavaScript",
+        Location: "Classroom 9",
+        StartDate: "11/22/22",
+        Fee: "350.00",
+    },
+    {
+        CourseId: "PROG300",
+        Title: "Introduction to Java",
+        Location: "Classroom 1",
+        StartDate: "01/09/23",
+        Fee: "50.00",
+    },
+    {
+        CourseId: "PROG400",
+        Title: "Introduction to SQL and Databases",
+        Location: "Classroom 7",
+        StartDate: "03/16/23",
+        Fee: "50.00",
+    },
+    {
+        CourseId: "PROJ500",
+        Title: "Introduction to Angular",
+        Location: "Classroom 1",
+        StartDate: "04/25/23",
+        Fee: "50.00",
+    }
+];
+
+
+// puts the list in order put the propertie in order
+function compareFn(a, b) {
+    if (a.Title < b.Title) {
+        return -1;
+    } else if (b.Title > a.Title) {
+        return 1;
+    }
+    // a must be equal to b
+    return 0;
+}
+courses.sort(compareFn);
+console.log(courses)
+
+
+
+courses.sort(compareFn);
+console.log(courses)
